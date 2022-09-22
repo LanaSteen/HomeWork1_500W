564030ef63a510af577cd8c7d15711b5309bbeeb
l4nst <l4nst@LAPTOP-HE9JLDVE>
2022-09-22 20:33:26 +0400

    Add project files.

---
+++ HomeWork1_500W/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1_500W
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////////////////////////// 1. სახელი და გვარი
+
+            //Console.WriteLine("Write your name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Write your surname");
+            //string surname = Console.ReadLine();
+            //Console.WriteLine("Write your age");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(name + " " + surname + " " + age + " years old");
+
+
+            ///////////////////////////////////////////////////////////////// 2. ორი რიცხვის ჯამი
+
+            //Console.WriteLine("first number");
+            //int firstNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("second number");
+            //int second = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("the sum of " + firstNum +" and "+ second +" equals " + (firstNum+second));
+
+
+
+
+            /////////////////////////////////////////////////////////////// 3. swap ადგილების გაცვლა
+
+            //Console.WriteLine("Enter the value of x: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Enter the value of y: ");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //    (x, y) = (y, x);
+            //Console.WriteLine("x equals: " + x + " and y equals: " + y);
+
+
+            /////////////////////////////////////////////////////////////// 4. 4 რიცხვის საშუალო
+
+            //int a, b, c, d;
+            //    a = Convert.ToInt32(Console.ReadLine());
+            //b = Convert.ToInt32(Console.ReadLine());
+            //c = Convert.ToInt32(Console.ReadLine());
+            //d = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Average equals: " + (a + b + c + d)/4);
+
+
+            /////////////////////////////////////////////////////////////// 5. კელვინის და ცელსიუსის კონვერტორი
+
+
+            //Console.WriteLine("Enter temperature in kelvin");
+            //Double kel = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Celsius equals: " + (kel - 273.15));
+
+
+            /////////////////////////////////////////////////////////////// 6. დადებითი თუ უარყოფითი
+
+
+
+            //Console.WriteLine("Enter the integer number");
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num > 0)
+            //{
+            //    Console.WriteLine("The number  " + num + " is Positive");
+            //}
+            //else if (num < 0)
+            //{
+            //    Console.WriteLine("The number  " + num + " is negative");
+            //}
+            //else if (num == 0)
+            //{
+            //    Console.WriteLine("The number  " + num + " is neither positive nor negative");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number is not integer");
+
+            //}
+
+
+            /////////////////////////////////////////////////////////////// 7. მაქსიმუმი და მინიმუმი
+
+            //Console.WriteLine("Enter the integer number 3 times");
+
+            //int num1, num2, num3;
+
+            //num1=Convert.ToInt32(Console.ReadLine());
+            //num2 = Convert.ToInt32(Console.ReadLine());
+            //num3 = Convert.ToInt32(Console.ReadLine());
+            //int[] list = { num1, num2, num3 };
+
+            //Console.WriteLine($"Max is: {list.Max()}");
+            //Console.WriteLine($"Min is: {list.Min()}");
+
+
+
+            ///////////////////////////////////////////////////////////// 8. თუ ტოლია გაზრდა 3ჯერ
+
+            //Console.WriteLine("Enter two equal integer number");
+            //int num1, num2;
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //num2 = Convert.ToInt32(Console.ReadLine());
+            //if (num1 == num2) 
+            //{
+            //    Console.WriteLine("The number your enter times 3 equals: " + (num1 * 3));
+
+            //}
+
+            ///////////////////////////////////////////////////////////// 9. არის თუ არა 7-ის ჯერადი
+
+            //Console.WriteLine("Enter integer number");
+            //int num1;
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //if ((num1 % 7) == 0)
+            //{
+            //    Console.WriteLine("The number your enter is 7's multiple");
+
+            //}
+            //else
+            //{
+            //        Console.WriteLine("The number your enter is not 7's multiple");
+            //}
+
+            ///////////////////////////////////////////////////////////// 10. შეიცავს თუ არა ციფრ 5-ს
+
+
+            //Console.WriteLine("Enter the number");
+            //string num1 = Console.ReadLine();
+            //char[] charArr = num1.ToCharArray();
+
+            //for (int i = 0; i < charArr.Length; i++)
+            //{
+            //    if (charArr[i] == '5')
+            //    {
+            //        Console.WriteLine("In Your number on index " + i +  " is the number 5 ");   
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("In Your number on index " + i + " is not the number 5 ");
+            //    }
+            //}
+
+
+            /////////////////////////////////////////////////////////////////// 11. რიცხვების განცალკევება
+
+
+            //Console.WriteLine("Enter the number");
+            //string num1 = Console.ReadLine();
+            //char[] charArr = num1.ToCharArray();
+            //string result;
+
+            //for (int i = 0; i < charArr.Length; i++)
+            //{
+
+            //    result = charArr[i] + " ";
+            //    Console.Write(result);
+            //}
+
+
+        }
+    }
+}
